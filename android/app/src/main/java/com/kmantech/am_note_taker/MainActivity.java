13ade5e368a98e3fa536f4093a26746e8caeeabf
Khushman Patel <khushman1@gmail.com>
2022-05-07 15:16:01 -0700

    Working through new project. Is run.

--- android/app/src/main/java/com/kmantech/am_note_taker/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.kmantech.am_note_taker;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/kmantech/am_note_taker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kmantech.am_note_taker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
